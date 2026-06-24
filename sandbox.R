50d24539f00eb852c1a692afffa2587465537bf6
Alayna Avila <alaynaavila2027@u.northwestern.edu>
2026-06-24 16:33:08 -0500

    cleaned variables for game data

--- sandbox.R
+++ sandbox.R
@@ -15,8 +15,27 @@ y_trends <- read_csv("data/yearly_trends.csv")
 # initial peek
 summary(games)
 
-# some variables are loaded as numeric rather than factor
+# some variables are loaded as numeric or character rather than factor
+# cleaning variables
 games <- games |>
   mutate(
-    
+    platform = as.factor(platform),
+    platform_type = as.factor(platform_type),
+    platform_maker = as.factor(platform_maker),
+    platform_generation = as.factor(platform_generation),
+    genre = as.factor(genre),
+    publisher = as.factor(publisher),
+    developer = as.factor(developer),
+    publisher_region = as.factor(publisher_region),
+    publisher_tier = as.factor(publisher_tier),
+    esrb_rating = as.factor(esrb_rating),
+    is_sequel = as.factor(is_sequel),
+    online_multiplayer = as.factor(online_multiplayer),
+    dlc_released = as.factor(dlc_released),
+    microtransactions = as.factor(microtransactions),
+    loot_boxes = as.factor(loot_boxes),
+    game_pass_available = as.factor(game_pass_available),
+    vr_support = as.factor(vr_support),
+    goty_nominated = as.factor(goty_nominated),
+    goty_won = as.factor(goty_won)
   )
