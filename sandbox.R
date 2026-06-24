618e3ba01391b14fb62282d4bf6f5d7ab7e5afee
Alayna Avila <alaynaavila2027@u.northwestern.edu>
2026-06-24 11:48:44 -0500

    loaded in the data

--- sandbox.R
+++ sandbox.R
@@ -1,6 +1,13 @@
-# Inital peek at the data
+# Initial peek at the data
 
 # load libraries
 library(tidyverse)
 
 # load data
+games <- read_csv("data/games.csv")
+genre_sum <- read_csv("data/genre_summary.csv")
+plat_sum <- read_csv("data/platform_summary.csv")
+pub_sum <- read_csv("data/publisher_summary.csv")
+vg_sales <- read_csv("data/video_games_sales.csv")
+y_trends <- read_csv("data/yearly_trends.csv")
+
