4323d148dfbb72305353016242c34398274ac4cd
Alayna Avila <alaynaavila2027@u.northwestern.edu>
2026-06-27 18:32:29 -0500

    finished looking at the game dataset and started the genre summary dataset

--- sandbox.R
+++ sandbox.R
@@ -41,25 +41,167 @@ games <- games |>
   )
 
 
+## Factor Variables ----
+
+# PC has the most games followed by mobile
 games |>
   count(platform) |>
   arrange(-n) |>
   print(n = 33)
 
-
+# large spread
 games |>
   ggplot(aes(x = genre)) +
   geom_bar()
 
+# T & M are most common
 games |>
   ggplot(aes(x = esrb_rating)) +
   geom_bar()
 
-games |>
-  ggplot(aes(x = esrb_rating)) +
-  geom_bar()
-
+# even-ish split
 games |>
   ggplot(aes(x = online_multiplayer)) +
   geom_bar()
 
+
+# about 2/3 aren't a sequel
+games |>
+  ggplot(aes(x = is_sequel)) +
+  geom_bar()
+
+# majority do have a dlc
+games |>
+  ggplot(aes(x = dlc_released)) +
+  geom_bar()
+
+# majority don't have microtransactions
+games |>
+  ggplot(aes(x = microtransactions)) +
+  geom_bar()
+
+# very few have loot boxes
+games |>
+  ggplot(aes(x = loot_boxes)) +
+  geom_bar()
+
+# very few are game pass available
+games |>
+  ggplot(aes(x = game_pass_available)) +
+  geom_bar()
+
+# very few support vr
+games |>
+  ggplot(aes(x = vr_support)) +
+  geom_bar()
+
+# very few were nominated for game of the year
+games |>
+  ggplot(aes(x = goty_nominated)) +
+  geom_bar()
+
+# even less won the award 
+games |>
+  ggplot(aes(x = goty_won)) +
+  geom_bar()
+
+
+## Numeric Variables ----
+
+# year of release - many between early 2000s and late 2010s
+games |>
+  ggplot(aes(x = year)) +
+  geom_boxplot()
+
+# slight skew to the left
+games |>
+  ggplot(aes(x = metacritic_score)) +
+  geom_density()
+
+# user score has a slight left skew
+games |>
+  ggplot(aes(x = user_score)) +
+  geom_histogram(bins = 50) +
+  geom_rug()
+
+# national sales has a heavy right skew
+games |>
+  ggplot(aes(x = na_sales_million)) +
+  geom_density()
+
+# similar to na sales
+games |>
+  ggplot(aes(x = eu_sales_million)) +
+  geom_density()
+
+# similar to na sales
+games |>
+  ggplot(aes(x = jp_sales_million)) +
+  geom_density()
+
+# similar to na sales
+games |>
+  ggplot(aes(x = other_sales_million)) +
+  geom_density()
+
+# similar to na sales
+games |>
+  ggplot(aes(x = global_sales_million)) +
+  geom_density()
+
+# similar to na sales
+games |>
+  ggplot(aes(x = estimated_revenue_million_usd)) +
+  geom_density()
+
+
+# pretty large range of prices $0-$80
+games |>
+  ggplot(aes(x = launch_price_usd)) +
+  geom_density() +
+  geom_rug()
+
+# heavy right skew
+games |>
+  ggplot(aes(x = how_long_to_beat_main_hrs)) +
+  geom_density()
+
+# similar to main hours
+games |>
+  ggplot(aes(x = how_long_to_beat_completionist_hrs)) +
+  geom_density()
+
+
+# Considering the large number of numeric variables with heavy skews, I will 
+# use power transformations to standardize the variables more for the potential
+# predictive models
+
+
+
+### 
+# Note: The other datasets have way less observations & will be examined with 
+# caution 
+###
+
+# Genre Summary ----
+
+## Initial Look ----
+summary(genre_sum)
+
+# none of the variables appear to be encoded incorrectly 
+# no obvious data cleansing so far
+
+## Variables ----
+
+# all variables of interest are numeric
+
+# not exactly sure on how to interpret this variable
+# given the context I will proceed under the impression that
+# this variable is the number of titles for all genres
+genre_sum |>
+  ggplot(aes(x = titles)) +
+  geom_histogram()
+
+genre_sum |>
+  ggplot(aes(x = titles, y = genre)) +
+  geom_boxplot()
