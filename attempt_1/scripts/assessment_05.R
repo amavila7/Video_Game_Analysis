b50921ac3fe0f3711276cafd88e6e88fe50711e2
Alayna Avila <alaynaavila2027@u.northwestern.edu>
2026-08-20 14:27:59 -0500

    beginning model assessment

---
+++ attempt_1/scripts/assessment_05.R
@@ -0,0 +1,16 @@
+# Model assessment ---- 
+
+# Load package(s)
+library(tidymodels)
+library(tidyverse)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load required objects here
+load(here("attempt_1/final_results/pred_log.rda"))
+load(here("attempt_1/final_results/pred_log.rda"))
+load(here("attempt_1/final_results/pred_log.rda"))
+load(here("attempt_1/final_results/pred_log.rda"))
+load(here("attempt_1/final_results/pred_log.rda"))
