d34ce2f56cbe75623b2ea4a3167de001ff72289e
Alayna Avila <alaynaavila2027@u.northwestern.edu>
2026-08-20 12:28:39 -0500

    finalizing results for models

---
+++ attempt_1/scripts/en_finalized_04.R
@@ -0,0 +1,59 @@
+# Train final model
+
+# Load package(s) & set seed ----
+library(tidymodels)
+library(tidyverse)
+library(here)
+
+# Handle conflicts
+tidymodels_prefer()
+
+# load required objects
+load(here("attempt_1/results/en_tuned.rda"))
+load(here("attempt_1/datasets/train_upsampled.rda"))
+load(here("attempt_1/datasets/game_testing.rda"))
+
+# finalize workflow ----
+final_wflow_en <- en_tuned |> 
+  extract_workflow(en_tuned) |>  
+  finalize_workflow(select_best(en_tuned, metric = "roc_auc"))
+
+# train final model ----
+# set seed
+set.seed(2183957)
+final_fit_en <- fit(final_wflow_en, train_upsampled)
+
+# saving final fit
+# save(final_fit_en, file = here("attempt_1results/final_fit.rda"))
+
+#################################################################
+# predicting
+
+game_test <- game_testing |>
+  mutate(
+    platform = as.factor(platform),
+    platform_type = as.factor(platform_type),
+    platform_maker = as.factor(platform_maker),
+    platform_generation = as.factor(platform_generation),
+    genre = as.factor(genre),
+    publisher_region = as.factor(publisher_region),
+    publisher_tier = as.factor(publisher_tier),
+    esrb_rating = as.factor(esrb_rating),
+    is_sequel = as.factor(is_sequel),
+    online_multiplayer = factor(online_multiplayer),
+    dlc_released = as.factor(dlc_released),
+    microtransactions = as.factor(microtransactions),
+    loot_boxes = as.factor(loot_boxes),
+    game_pass_available = as.factor(game_pass_available),
+    vr_support = as.factor(vr_support),
+    goty_nominated = as.factor(goty_nominated),
+    goty_won = as.factor(goty_won)
+  )
+
+pred_en <- game_test |>
+  bind_cols(predict(final_fit_en, new_data = game_test, type = "prob")) |>
+  select(game_id, predicted = .pred_1)
+
+
+# save results
+save(pred_en, file = "attempt_1/final_results/pred_en.rda")
